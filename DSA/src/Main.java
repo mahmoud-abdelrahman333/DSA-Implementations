432570e88f11137813778acad259c82e58c985b9
mahmoud-abdelrahman333 <mahmoudabdelrahman713@gmail.com>
2024-10-23 16:19:53 +0300

    DSA

---
+++ DSA/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
